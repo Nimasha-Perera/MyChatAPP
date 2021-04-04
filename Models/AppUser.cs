6007fdc4e95f594c85d029fe5261b0fcc8f7d6eb
Nimasha-Perera <73678949+Nimasha-Perera@users.noreply.github.com>
2021-04-04 08:58:51 +0530

    Add files via upload

---
+++ Models/AppUser.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace MyChatApp.Models
+{
+    public class AppUser : IdentityUser 
+    {
+        public AppUser()
+        {
+            Messages = new HashSet<Message>();
+        }
+        // 1-* AppUser || Message 
+        public virtual ICollection <Message>Messages { get; set; }
+
+    }
+}
