665427c025baa918b35f25ef981072dd9df7a8b9
Nimasha-Perera <73678949+Nimasha-Perera@users.noreply.github.com>
2021-04-04 08:59:26 +0530

    Add files via upload

---
+++ Hubs/ChatHub.cs
@@ -0,0 +1,14 @@
+﻿using MyChatApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyChatApp.Hubs
+{
+    public class ChatHub : Hub
+    { public async Task SendMessage(Message meassage )= >
+            await Clients.All.SendAsync("receiveMessage",message);
+             
+    }
+}
